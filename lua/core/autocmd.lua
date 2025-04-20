bef2f471c2e3fdb7068ef5d6690426693f41472c
ken85illo <pabillokenneth81@gmail.com>
2025-04-21 00:44:28 +0800

    fix persistence not working with neotree

--- lua/core/autocmd.lua
+++ lua/core/autocmd.lua
@@ -7,3 +7,12 @@ api.nvim_create_autocmd('TextYankPost', {
         vim.highlight.on_yank()
     end,
 })
+
+-- Disable neo-tree when leaving (it conflicts with persistence)
+api.nvim_create_autocmd('VimLeavePre', {
+   desc = 'Disable neo-tree and save persistence',
+   callback = function()
+       vim.cmd("Neotree close <CR>")
+       require("persistence").save()
+   end,
+})
