68d00099955c2f66937bddde41204626e3bd899d
ken85illo <pabillokenneth81@gmail.com>
2025-04-21 00:45:41 +0800

    set leader to space

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -1,4 +1,10 @@
 local km = vim.keymap
+local opt = vim.opt
+
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
+
+vim.keymap.set({ 'n', 'v' }, '<Space>', '<Nop>', { silent = true })
 
 km.set('n', '<Esc>', '<cmd>nohlsearch<CR>') -- Clear highlights on search when pressing <Esc> in normal mode
 
@@ -7,7 +13,3 @@ km.set('n', '<C-h>', '<C-w><C-h>', { desc = 'Move focus to the left window' })
 km.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
 km.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
 km.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
-km.set("n", "<C-S-h>", "<C-w>H", { desc = "Move window to the left" })
-km.set("n", "<C-S-l>", "<C-w>L", { desc = "Move window to the right" })
-km.set("n", "<C-S-j>", "<C-w>J", { desc = "Move window to the lower" })
-km.set("n", "<C-S-k>", "<C-w>K", { desc = "Move window to the upper" })
