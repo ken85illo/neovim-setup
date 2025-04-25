c472c7e64da29311bad6ffbef346ae27a3f1fa66
ken85illo <pabillokenneth81@gmail.com>
2025-04-26 02:28:50 +0800

    yayy i have lsp now

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -1,19 +1,57 @@
 return {
-    "williamboman/mason-lspconfig.nvim",
+    'williamboman/mason-lspconfig.nvim',
     dependencies = {
-        "williamboman/mason.nvim",
-        "neovim/nvim-lspconfig",
+        'williamboman/mason.nvim',
+        'neovim/nvim-lspconfig',
+        'hrsh7th/cmp-nvim-lsp',
     },
     config = function()
-        require("mason").setup()
-        require("mason-lspconfig").setup {
+        -- [[Set-up using LSP Zero]]
+        -- See https://lsp-zero.netlify.app/docs/tutorial.html
+
+        -- Add cmp_nvim_lsp capabilities settings to lspconfig
+        local lspconfig_defaults = require('lspconfig').util.default_config
+        lspconfig_defaults.capabilities = vim.tbl_deep_extend('force', lspconfig_defaults.capabilities, require('cmp_nvim_lsp').default_capabilities())
+
+        -- vim.diagnostic.config({virtual_text = true,})
+
+        vim.api.nvim_create_autocmd('LspAttach', {
+            desc = 'LSP actions',
+            callback = function(event)
+                local opts = { buffer = event.buf }
+
+                vim.keymap.set('n', 'K', '<cmd>lua vim.lsp.buf.hover()<cr>', opts)
+                vim.keymap.set('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<cr>', opts)
+                vim.keymap.set('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<cr>', opts)
+                vim.keymap.set('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<cr>', opts)
+                vim.keymap.set('n', 'go', '<cmd>lua vim.lsp.buf.type_definition()<cr>', opts)
+                vim.keymap.set('n', 'gr', '<cmd>lua vim.lsp.buf.references()<cr>', opts)
+                vim.keymap.set('n', 'gs', '<cmd>lua vim.lsp.buf.signature_help()<cr>', opts)
+                vim.keymap.set('n', '<F2>', '<cmd>lua vim.lsp.buf.rename()<cr>', opts)
+                vim.keymap.set({ 'n', 'x' }, '<F3>', "<cmd>lua require('conform').format()<cr>", opts)
+                vim.keymap.set('n', '<F4>', '<cmd>lua vim.lsp.buf.code_action()<cr>', opts)
+            end,
+        })
+
+        -- Mason lspconfig setup
+        require('mason').setup {
+            ui = {
+                border = 'rounded',
+                size = {
+                    width = 0.8,
+                    height = 0.8,
+                },
+            },
+        }
+
+        require('mason-lspconfig').setup {
             ensure_installed = {
-                -- Lua
-                "lua_ls",
-            }
+                'lua_ls', -- Lua
+            },
+            automatic_installation = true,
         }
 
         -- [[Setups for LSPs]]
-        require'lspconfig'.lua_ls.setup{}
+        require('lspconfig').lua_ls.setup {}
     end,
 }
