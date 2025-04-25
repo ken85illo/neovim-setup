66bcb8cb61a76431715ed72943440d6671676da3
ken85illo <pabillokenneth81@gmail.com>
2025-04-26 02:24:36 +0800

    define autocmd

--- lua/core/autocmd.lua
+++ lua/core/autocmd.lua
@@ -1,7 +1,9 @@
 local api = vim.api
+local autocmd = api.nvim_create_autocmd
+local augroup = api.nvim_create_augroup
 
 -- Display a highlight when yanking text
-api.nvim_create_autocmd('TextYankPost', {
+autocmd('TextYankPost', {
     desc = 'Highlight when yanking (copying) text',
     callback = function()
         vim.highlight.on_yank()
@@ -9,28 +11,30 @@ api.nvim_create_autocmd('TextYankPost', {
 })
 
 -- Disable neo-tree when leaving (it conflicts with persistence)
-api.nvim_create_autocmd('VimLeavePre', {
-   desc = 'Disable neo-tree and save persistence',
-   callback = function()
-       vim.cmd("Neotree close <CR>")
-       require("persistence").save()
-   end,
-})
-
--- More efficient quitting method when opening temporary windows
-vim.api.nvim_create_autocmd("FileType", {
-  pattern = { "help", "man", "qf", "lspinfo", "checkhealth", "spectre_panel" },
-  callback = function()
-    vim.keymap.set("n", "q", "<cmd>q<CR>", {buffer = true, silent = true })
-  end,
-})
-
--- Make background transparent
-api.nvim_create_autocmd({ "ColorScheme", "BufWinEnter", "WinNew"}, {
-    desc = "Fix all backgrounds",
+autocmd('VimLeavePre', {
+    desc = 'Disable neo-tree and save persistence',
     callback = function()
-        api.nvim_set_hl(0, "WinSeparator", { fg = "#27354c" }) -- Show Window Borders
-        api.nvim_set_hl(0, "NormalFloat", { bg = "NONE" }) -- Transparent Float Windows
+        vim.cmd 'Neotree close <CR>'
+        require('persistence').save()
     end,
 })
 
+-- More efficient quitting method when opening temporary windows
+autocmd('FileType', {
+    pattern = { 'help', 'man', 'qf', 'lspinfo', 'checkhealth', 'spectre_panel', 'vim' },
+    callback = function()
+        vim.keymap.set('n', 'q', '<cmd>q<CR>', { buffer = true, silent = true })
+    end,
+})
+
+-- Make background transparent
+autocmd({ 'ColorScheme', 'BufWinEnter', 'WinNew' }, {
+    desc = 'Transparent background',
+    pattern = '*',
+    callback = function()
+        api.nvim_set_hl(0, 'WinSeparator', { fg = '#27354c' }) -- Show Window Borders
+        api.nvim_set_hl(0, 'NormalFloat', { bg = 'NONE' }) -- Transparent Float Windows
+        api.nvim_set_hl(0, 'TabLineFill', { bg = 'NONE' }) -- Transparent Buffer Line
+        api.nvim_set_hl(0, 'TabLine', { bg = 'NONE' }) -- Transparent Buffer Line
+    end,
+})
