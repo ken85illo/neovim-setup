fe83e66e827a8c344049b035ca9694cf8468ac15
ken85illo <pabillokenneth81@gmail.com>
2025-04-26 02:30:04 +0800

    save session plugin

--- lua/plugins/persistence.lua
+++ lua/plugins/persistence.lua
@@ -1,7 +1,7 @@
 return {
-  "folke/persistence.nvim",
-  event = "BufReadPre", -- this will only start session saving when an actual file was opened
-  config = function()
-      require("persistence").setup()
+    'folke/persistence.nvim',
+    event = 'BufReadPre', -- this will only start session saving when an actual file was opened
+    config = function()
+        require('persistence').setup()
     end,
 }
