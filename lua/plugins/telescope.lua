cf3c9e061e7fcd9d858827be342da431d12d5800
ken85illo <pabillokenneth81@gmail.com>
2025-04-26 02:28:28 +0800

    epic plugins for navigation lol

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,29 +1,35 @@
 return {
-    'nvim-telescope/telescope.nvim', tag = '0.1.8',
-    event = "VimEnter",
+    'nvim-telescope/telescope.nvim',
+    tag = '0.1.8',
+    event = 'VimEnter',
     dependencies = {
+        'nvim-lua/plenary.nvim',
+        'nvim-telescope/telescope-ui-select.nvim',
         {
-            'nvim-lua/plenary.nvim'
-        },
-        {
-          "1riz/telescope-macros.nvim",
-          config = function()
-            require("telescope").load_extension "macros"
-          end
+            '1riz/telescope-macros.nvim',
+            config = function()
+                require('telescope').load_extension 'macros'
+            end,
         },
     },
-    config = function() 
-        local actions = require('telescope.actions')
-        
-        require('telescope').setup{
+    config = function()
+        local actions = require 'telescope.actions'
+
+        require('telescope').setup {
             defaults = {
-                --initial_mode = "normal",
+                initial_mode = 'normal',
                 mappings = {
-                  n = {
-                    ["q"] = actions.close,
-                  },
+                    n = {
+                        ['q'] = actions.close,
+                    },
                 },
-              }
+            },
+            extensions = {
+                ['ui-select'] = {
+                    require('telescope.themes').get_dropdown {},
+                },
+            },
         }
+        require('telescope').load_extension 'ui-select'
     end,
 }
--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,18 +1,17 @@
 return {
     'nvim-treesitter/nvim-treesitter',
     config = function()
-        require'nvim-treesitter.configs'.setup {
-          ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "markdown", "markdown_inline" },
+        require('nvim-treesitter.configs').setup {
+            ensure_installed = { 'c', 'lua', 'vim', 'vimdoc', 'query', 'markdown', 'markdown_inline' },
 
-          sync_install = false,
-          auto_install = true,
-          ignore_install = { "javascript" },
+            sync_install = false,
+            auto_install = true,
+            ignore_install = { 'javascript' },
 
-          highlight = {
-            enable = true,
-            additional_vim_regex_highlighting = false,
-          },
+            highlight = {
+                enable = true,
+                additional_vim_regex_highlighting = false,
+            },
         }
     end,
-
 }
