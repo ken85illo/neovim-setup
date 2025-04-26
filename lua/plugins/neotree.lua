a922197bfb53468cd14f9842948a6f44c1e022db
ken85illo <pabillokenneth81@gmail.com>
2025-04-26 14:59:58 +0800

    neo-tree show hidden files

--- lua/plugins/neotree.lua
+++ lua/plugins/neotree.lua
@@ -1,17 +1,20 @@
 return {
-    "nvim-neo-tree/neo-tree.nvim",
-    branch = "v3.x",
+    'nvim-neo-tree/neo-tree.nvim',
+    branch = 'v3.x',
     dependencies = {
-        "nvim-lua/plenary.nvim",
-        "nvim-tree/nvim-web-devicons",
-        "MunifTanjim/nui.nvim",
+        'nvim-lua/plenary.nvim',
+        'nvim-tree/nvim-web-devicons',
+        'MunifTanjim/nui.nvim',
     },
     opts = {
         filesystem = {
             use_libuv_file_watcher = true,
+            filtered_items = {
+                visible = true,
+            },
         },
         source_selector = {
             statusline = true,
-        }
+        },
     },
 }
