dc25831bb3db1222abcc4b13ba179e1a8fd3587a
ken85illo <pabillokenneth81@gmail.com>
2025-04-26 20:49:57 +0800

    ui changes

--- lua/plugins/noice.lua
+++ lua/plugins/noice.lua
@@ -7,7 +7,7 @@ return {
             'rcarriga/nvim-notify',
             opts = {
                 background_colour = '#000000',
-                max_width = 50,
+                max_width = 40,
             },
         },
     },
