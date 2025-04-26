52e9a4a1497508218bd456765d92c40c28bf9034
ken85illo <pabillokenneth81@gmail.com>
2025-04-26 20:07:58 +0800

    add window-picker plugin

--- init.lua
+++ init.lua
@@ -3,33 +3,33 @@ require 'core.keymaps'
 require 'core.autocmd'
 
 -- Bootstrap lazy.nvim
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
-  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
-  local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
-  if vim.v.shell_error ~= 0 then
-    vim.api.nvim_echo({
-      { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
-      { out, "WarningMsg" },
-      { "\nPress any key to exit..." },
-    }, true, {})
-    vim.fn.getchar()
-    os.exit(1)
-  end
+    local lazyrepo = 'https://github.com/folke/lazy.nvim.git'
+    local out = vim.fn.system { 'git', 'clone', '--filter=blob:none', '--branch=stable', lazyrepo, lazypath }
+    if vim.v.shell_error ~= 0 then
+        vim.api.nvim_echo({
+            { 'Failed to clone lazy.nvim:\n', 'ErrorMsg' },
+            { out, 'WarningMsg' },
+            { '\nPress any key to exit...' },
+        }, true, {})
+        vim.fn.getchar()
+        os.exit(1)
+    end
 end
 vim.opt.rtp:prepend(lazypath)
 
 -- See :h lazy.nvim-plugin-spec
-require("lazy").setup({
+require('lazy').setup {
     spec = {
-        { import = "plugins" }, -- Import lua files inside plugins folder
-      },
+        { import = 'plugins' }, -- Import lua files inside plugins folder
+    },
     ui = {
-        border = "rounded",
+        border = 'rounded',
         size = {
             width = 0.8,
             height = 0.8,
-		},
-	},
+        },
+    },
     checker = { enabled = true },
-})
+}
--- lua/plugins/neotree.lua
+++ lua/plugins/neotree.lua
@@ -5,6 +5,22 @@ return {
         'nvim-lua/plenary.nvim',
         'nvim-tree/nvim-web-devicons',
         'MunifTanjim/nui.nvim',
+        {
+            's1n7ax/nvim-window-picker',
+            version = '2.*',
+            config = function()
+                require('window-picker').setup {
+                    filter_rules = {
+                        include_current_win = false,
+                        autoselect_one = true,
+                        bo = {
+                            filetype = { 'neo-tree', 'neo-tree-popup', 'notify' },
+                            buftype = { 'terminal', 'quickfix' },
+                        },
+                    },
+                }
+            end,
+        },
     },
     opts = {
         filesystem = {
