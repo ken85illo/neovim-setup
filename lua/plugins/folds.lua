402d8d76201c7d433666365f6585ebc8f552ec48
ken85illo <pabillokenneth81@gmail.com>
2025-09-01 20:43:01 +0800

    fix: stop automatic code folding

--- lua/plugins/folds.lua
+++ lua/plugins/folds.lua
@@ -44,6 +44,10 @@ return {
         },
         init = function()
             vim.o.statuscolumn = '%=%l%s%#FoldColumn#%{foldlevel(v:lnum) > foldlevel(v:lnum - 1) ? (foldclosed(v:lnum) == -1 ? " " : " ") : "  " }%*'
+            vim.o.foldcolumn = '1' -- '0' is not bad
+            vim.o.foldlevel = 99 -- Using ufo provider need a large value, feel free to decrease the value
+            vim.o.foldlevelstart = 99
+            vim.o.foldenable = true
         end,
         config = function()
             local handler = function(virtText, lnum, endLnum, width, truncate)
